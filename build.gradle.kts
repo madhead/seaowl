c9d3681b7e1df0274e7e65cfb424873bfc683563
madhead <siarhei.krukau@gmail.com>
2018-02-18 23:03:31 +0300

    Update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ buildscript {
 	}
 
 	dependencies {
-		classpath("org.jetbrains.kotlin:kotlin-native-gradle-plugin:0.5")
+		classpath("org.jetbrains.kotlin:kotlin-native-gradle-plugin:0.6")
 	}
 }
 
 task<Wrapper>("wrapper") {
-	gradleVersion = "4.4"
+	gradleVersion = "4.5"
 	distributionType = Wrapper.DistributionType.ALL
 }
